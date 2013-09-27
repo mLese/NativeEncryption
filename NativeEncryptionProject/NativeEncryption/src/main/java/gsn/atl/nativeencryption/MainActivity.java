ae9c36b956d268f478be1092ebc85f4e61a4a490
Michael Lese <michael.lese@echostar.com>
2013-09-27 17:17:59 -0400

    Modularize
    Modularizing things a little bit

--- NativeEncryptionProject/NativeEncryption/src/main/java/gsn/atl/nativeencryption/MainActivity.java
+++ NativeEncryptionProject/NativeEncryption/src/main/java/gsn/atl/nativeencryption/MainActivity.java
@@ -59,7 +59,7 @@ public class MainActivity extends Activity {
     };
 
     private void encryptLibrary(){
-        // encrypt using java libs
+        Encryption.encryptLibrary();
     }
 
     View.OnClickListener encryptNativeListener = new View.OnClickListener() {
